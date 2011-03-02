8fda545d3ad535a1f233d582cf67eaf692f159b5
ahmadnassri <ahmad@codeinchaos.com>
2011-03-02 23:32:23 +0000

    adding proper command line syntax

---
+++ generate.php
@@ -0,0 +1,164 @@
+<?php
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(dirname(__FILE__) . '/library'),
+    get_include_path()
+)));
+
+require_once 'Zend/Loader/Autoloader.php';
+
+$autoloader = Zend_Loader_Autoloader::getInstance();
+$autoloader->registerNamespace('Chaos_');
+
+// Setup the CLI Commands
+try
+{
+    $opts = new Zend_Console_Getopt(array(
+        'host|h=s'      => 'host option, required string parameter',
+        'port|p-i'      => 'port option, optional integer parameter',
+        'database|d=s'  => 'database option, required word parameter',
+        'username|u=s'  => 'username option, required string parameter',
+        'password|P-s'  => 'password option, optional string parameter',
+        'output|o=s'    => 'output directory option, required string parameter',
+        'prefix-s'      => 'prefix option, optional string parameter',
+        'verbose|v'     => 'Print verbose output',
+        'help'          => 'help option'
+    ));
+
+    $opts->parse();
+}
+catch (Zend_Console_Getopt_Exception $e)
+{
+    exit($e->getMessage() ."\n\n". $e->getUsageMessage());
+}
+
+if (isset($opts->help) or !isset($opts->host, $opts->database, $opts->username, $opts->output))
+{
+    echo $opts->getUsageMessage();
+    exit;
+}
+
+if (!isset($opts->port))
+{
+    $opts->port = 3306;
+}
+
+$parser = new Chaos_Db_Table_Parser();
+
+if (isset($opts->prefix))
+{
+    $parser->setPrefix($opts->prefix);
+}
+
+$adapter = new Zend_Db_Adapter_Pdo_Mysql(array(
+    'host'      => $opts->host,
+    'dbname'    => $opts->database,
+    'port'      => $opts->port,
+    'username'  => $opts->username,
+    'password'  => $opts->password,
+    'charset'   => 'UTF8'
+));
+
+Zend_Db_Table::setDefaultAdapter($adapter);
+
+$tables = $adapter->listTables();
+
+foreach ($tables as $name)
+{
+    $table = new Zend_Db_Table($name);
+    $info = $parser->parse($table);
+
+    foreach ($info['uniques'] as $key)
+    {
+        $info['methods'][] = new Zend_CodeGenerator_Php_Method(array(
+            'name' => sprintf('findBy%s', $parser->formatMethodName($key)),
+            'body' => sprintf('return $this->fetchAll($this->select()->where(\'%s = ?\', $value));', $key),
+            'parameters' => array(
+                array(
+                    'name' => 'value'
+                ),
+            ),
+        ));
+
+        $info['methods'][] = new Zend_CodeGenerator_Php_Method(array(
+            'name' => sprintf('countBy%s', $parser->formatMethodName($key)),
+            'body' => sprintf('return $this->fetchRow($this->select()->from($this->_name, array(\'%s\', \'num\'=> \'COUNT(*)\'))->where(\'%s = ?\', $value))->num;', $key, $key),
+            'parameters' => array(
+                array(
+                    'name' => 'value'
+                ),
+            ),
+        ));
+    }
+
+    foreach ($info['parents'] as $table => $key)
+    {
+        $info['methods'][] = new Zend_CodeGenerator_Php_Method(array(
+            'name' => sprintf('find%s', $parser->formatMethodName($table)),
+            'body' => sprintf('return $this->findParentRow(new %s(), null, $select);', $parser->formatClassName($table)),
+            'parameters' => array(
+                array(
+                    'name' => 'select',
+                    'defaultValue' => null
+                ),
+            ),
+        ));
+    }
+
+    foreach ($info['dependants'] as $table)
+    {
+        $info['methods'][] = new Zend_CodeGenerator_Php_Method(array(
+            'name' => sprintf('find%s', $parser->formatMethodName($table)),
+            'body' => sprintf('return $this->findDependentRowset(new %s(), null, $select);', $parser->formatClassName($table)),
+            'parameters' => array(
+                array(
+                    'name' => 'select',
+                    'defaultValue' => null
+                ),
+            ),
+        ));
+    }
+
+    $file = new Zend_CodeGenerator_Php_File(array(
+        'classes' => array(
+            new Zend_CodeGenerator_Php_Class(array(
+                'name' => $parser->formatClassName($name),
+                'methods' => $info['methods'],
+                'properties' => array(
+                    array (
+                        'name'          => '_name',
+                        'visibility'    => 'protected',
+                        'defaultValue'  => $name
+                    ),
+
+                    array (
+                        'name'          => '_primary',
+                        'visibility'    => 'protected',
+                        'defaultValue'  => array_values($info['primary'])
+                    ),
+
+                    array (
+                        'name'          => '_rowClass',
+                        'visibility'    => 'protected',
+                        'defaultValue'  => 'LS_Db_Table_Row_Abstract'
+                    ),
+
+                    array (
+                        'name'          => '_referenceMap',
+                        'visibility'    => 'protected',
+                        'defaultValue'  => $info['referenceMap']
+                    ),
+
+                    array (
+                        'name'          => '_dependentTables',
+                        'visibility'    => 'protected',
+                        'defaultValue'  => array()
+                    ),
+                ),
+
+                'extendedClass' => 'Zend_Db_Table_Abstract'
+            )),
+        )
+    ));
+
+    file_put_contents($opts->output. $parser->formatTableName($name) . '.php', $file->generate());
+}
---
+++ library/Chaos/Db/Table/Parser.php
@@ -0,0 +1,131 @@
+<?php
+
+class Chaos_Db_Table_Parser
+{
+    private $_prefix = null;
+
+    public function __construct()
+    {
+    }
+
+    public function parse(Zend_Db_Table_Abstract $table)
+    {
+        $info = $table->info();
+
+        $info['uniques'] = array();
+        $info['parents'] = array();
+        $info['dependants'] = array();
+
+        $adapter = $table->getAdapter();
+
+        foreach ($info['metadata'] as $property => $details)
+        {
+            // match php types
+            $info['phptypes'][$property] = $this->convertMysqlTypeToPhp($details['DATA_TYPE']);
+
+            // find uniques
+            $tmp = $adapter->fetchRow('DESCRIBE `'.$info['name'].'` `'.$property.'`;');
+
+            if (!empty($tmp['Key']))
+            {
+                $info['uniques'][$property] = $property;
+            }
+        }
+
+        // get foreign keys
+        $result = $adapter->fetchAll('SHOW CREATE TABLE `' . $info['name'].'`');
+        $query = $result[0]['Create Table'];
+        $lines = explode("\n", $query);
+        $tblinfo = array();
+        $keys = array();
+
+        foreach ($lines as $line) {
+            preg_match('/^\s*CONSTRAINT `(\w+)` FOREIGN KEY \(`(\w+)`\) REFERENCES `(\w+)` \(`(\w+)`\)/', $line, $tblinfo);
+
+            if (sizeof($tblinfo) > 0)
+            {
+                $keys[] = $tmp = array(
+                    'key'       => $tblinfo[1],
+                    'column'    => $tblinfo[2],
+                    'fk_table'  => $tblinfo[3],
+                    'fk_column' => $tblinfo[4]
+                );
+
+                $info['referenceMap'][$tmp['key']] = array(
+                    'columns' => $tmp['column'],
+                    'refTableClass' => $this->formatClassName($tmp['fk_table']),
+                    'refColumns' => $tmp['fk_column']
+                );
+
+                $info['parents'][$tmp['fk_table']] = $tmp['key'];
+
+/*
+                $info['dependentTables']
+*/
+            }
+        }
+
+        $info['foreign_keys'] = $keys;
+
+        return $info;
+    }
+
+    public function setPrefix($string)
+    {
+        $this->_prefix = $string;
+    }
+
+    public function getPrefix()
+    {
+        return $this->_prefix;
+    }
+
+    public function formatTableName($name)
+    {
+        return str_replace('_','', mb_convert_case($name, MB_CASE_TITLE));
+    }
+
+    public function formatClassName($name)
+    {
+        $prefix = $this->getPrefix();
+
+        if (empty($prefix))
+        {
+            return sprintf('Model_%s', $this->formatTableName($name));
+        }
+        else
+        {
+            return sprintf('%s_Model_%s', $prefix, $this->formatTableName($name));
+        }
+    }
+
+    public static function formatMethodName($string)
+    {
+        return str_replace('_','', mb_convert_case($string, MB_CASE_TITLE));
+    }
+
+    /**
+     * map mysql data types to php data types
+     * @param string $mysqlType
+     * @return string
+     */
+    protected function convertMysqlTypeToPhp($mysqlType)
+    {
+
+        $type = 'string';
+
+        // integers
+        if (preg_match('#^(.*)int(.*)$#', $mysqlType))
+        {
+            $type = 'int';
+        }
+
+        if (preg_match('#^(.*)float(.*)$#', $mysqlType))
+        {
+            $type = 'float';
+        }
+
+        return $type;
+    }
+
+}
